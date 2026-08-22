c2cb9b0b6601de3b105bfb2a99058b03f3ff2d52
andilehlutwa3251-hue <andilehlutwa3251@gmail.com>
2026-08-22 21:22:18 +0200

    fix: update Stripe API version and add env guard

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,11 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2022-11-15',
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('STRIPE_SECRET_KEY is not set');
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2024-06-20',
 });
 
 export default stripe;
