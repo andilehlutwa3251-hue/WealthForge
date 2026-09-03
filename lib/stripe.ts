c367b91d777fb84bd57c7f8407cc8719382ff521
andilehlutwa3251-hue <andilehlutwa3251@gmail.com>
2026-09-03 20:27:14 +0000

    fix: lazy-init Stripe client so build doesn't require STRIPE_SECRET_KEY at build time

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,11 +1,15 @@
 import Stripe from 'stripe';
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('STRIPE_SECRET_KEY is not set');
+let _stripe: Stripe | null = null;
+
+export function getStripe(): Stripe {
+  if (!_stripe) {
+    if (!process.env.STRIPE_SECRET_KEY) {
+      throw new Error('STRIPE_SECRET_KEY is not set');
+    }
+    _stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: '2022-11-15',
+    });
+  }
+  return _stripe;
 }
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2022-11-15',
-});
-
-export default stripe;
